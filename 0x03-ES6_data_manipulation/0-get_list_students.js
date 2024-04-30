5c6eabab40aff099ce5bdeef868e28bcfa9fdc2b
elyton250 <nelysemathieu@gmail.com>
2024-04-30 13:43:19 +0200

    types

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+    return [
+        {id: 1, firstName: 'Guillaume', location: 'San Franciso'},
+        {id: 2, lfirstName: 'James', ocation: 'Colombia'},
+        {id: 5, firstName: 'Serena', location: 'San Franciso'}
+    ]
+};
